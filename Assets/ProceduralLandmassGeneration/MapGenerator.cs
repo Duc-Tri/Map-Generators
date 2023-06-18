f51877cf5ba62e1afda730c72361e7f950645c28
Tri <ductri.vu@gmail.com>
2023-06-18 14:01:14 +0200

    add color map (water, grass, snow,...)

--- Assets/ProceduralLandmassGeneration/MapGenerator.cs
+++ Assets/ProceduralLandmassGeneration/MapGenerator.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ProceduralLandmassGeneration
 {
+    [Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
+    }
 
     public class MapGenerator : MonoBehaviour
     {
+        public enum DrawMode { NoiseMap, ColorMap };
+        public DrawMode drawMode;
+
         public int mapWidth;
         public int mapHeight;
         public float noiseScale;
@@ -22,13 +33,34 @@ namespace ProceduralLandmassGeneration
 
 
         public bool autoUpdate;
+        public TerrainType[] regions;
 
         public void GenerateMap()
         {
             float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+            Color[] colorMap = new Color[mapHeight * mapWidth];
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    float currentHeight = noiseMap[x, y];
+                    for (int i = 0; i < regions.Length; i++)
+                    {
+                        if (currentHeight <= regions[i].height)
+                        {
+                            colorMap[y * mapWidth + x] = regions[i].color;
+                            break;
+                        }
+                    }
+                }
+            }
+
             MapDisplay display = FindAnyObjectByType<MapDisplay>();
-            display.DrawNoiseMap(noiseMap);
+            if (drawMode == DrawMode.NoiseMap)
+                display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+            else if (drawMode == DrawMode.ColorMap)
+                display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
         }
 
         private void OnValidate()
