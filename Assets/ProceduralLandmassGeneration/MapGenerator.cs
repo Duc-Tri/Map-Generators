757c37e26ca84e1e12428d5ec93363ec560868e4
Tri <ductri.vu@gmail.com>
2023-06-18 19:14:33 +0200

    AnimationCurve for heightMultiplier, LOD (Level Of Detail) mesh generation

--- Assets/ProceduralLandmassGeneration/MapGenerator.cs
+++ Assets/ProceduralLandmassGeneration/MapGenerator.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 
 namespace ProceduralLandmassGeneration
 {
-    [Serializable]
-    public struct TerrainType
-    {
-        public string name;
-        public float height;
-        public Color color;
-    }
-
     public class MapGenerator : MonoBehaviour
     {
         public enum DrawMode { NoiseMap, ColorMap, Mesh };
         public DrawMode drawMode;
 
-        public int mapWidth;
-        public int mapHeight;
+        [Range(0, 6)]
+        public int levelOfDetail; // 1=no modification
+        // Level Of Detail
+        const int mapChunkSize = 241; // factor 2,4,6,8,10,12 nicely
+
         public float noiseScale;
 
         public int octaves;
@@ -30,25 +25,27 @@ namespace ProceduralLandmassGeneration
 
         public int seed;
         public Vector2 offset;
+        public float meshHeightMultiplier;
+        public AnimationCurve meshHeightCurve;
 
         public bool autoUpdate;
         public TerrainType[] regions;
 
         public void GenerateMap()
         {
-            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+            float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-            Color[] colorMap = new Color[mapHeight * mapWidth];
-            for (int y = 0; y < mapHeight; y++)
+            Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+            for (int y = 0; y < mapChunkSize; y++)
             {
-                for (int x = 0; x < mapWidth; x++)
+                for (int x = 0; x < mapChunkSize; x++)
                 {
                     float currentHeight = noiseMap[x, y];
                     for (int i = 0; i < regions.Length; i++)
                     {
                         if (currentHeight <= regions[i].height)
                         {
-                            colorMap[y * mapWidth + x] = regions[i].color;
+                            colorMap[y * mapChunkSize + x] = regions[i].color;
                             break;
                         }
                     }
@@ -59,20 +56,26 @@ namespace ProceduralLandmassGeneration
             if (drawMode == DrawMode.NoiseMap)
                 display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
             else if (drawMode == DrawMode.ColorMap)
-                display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+                display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
             else if (drawMode == DrawMode.Mesh)
-                display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+                display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
+                    TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
         }
 
         private void OnValidate()
         {
-            if (mapWidth < 1) mapWidth = 1;
-
-            if (mapHeight < 1) mapHeight = 1;
-
             if (lacunarity < 1) lacunarity = 1;
 
             if (octaves < 0) octaves = 1;
         }
     }
+
+    [Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
+    }
+
 }
